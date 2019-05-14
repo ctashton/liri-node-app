d00bc01a3f11674063b513d22483f84cce60384e
kidffiti <coreyash10@gmail.com>
2019-05-14 05:39:45 -0400

    Bonus portion complete.  Still need to finish readMe

--- liri.js
+++ liri.js
@@ -20,10 +20,10 @@ var args = process.argv
 var command = args[2]
 var input = args.slice(3).join(" ");
 var artist;
-var movie;
-var song;
+liri();
 console.log(input)
-switch(command){
+function liri(){ 
+    switch(command){
     case "concert-this":
         concertThis(input);
         break;
@@ -35,14 +35,17 @@ switch(command){
         }
             break;
     case "movie-this":
-    if (input){
-        movieThis(input);;
-    } else{
-        movieThis("Mr. Nobody")
-            console.log("If you haven't watched 'Mr. Nobody' then you should: <http://www.imdb.com/title/tt0485947/>"),
-            console.log("It's on Netflix!")
-    }
-
+        if (input){
+            movieThis(input);
+        } else{
+            movieThis("Mr. Nobody")
+                console.log("If you haven't watched 'Mr. Nobody' then you should: <http://www.imdb.com/title/tt0485947/>"),
+                console.log("It's on Netflix!")
+        }
+        break;
+    case "do-what-it-says":
+        doWhatItSays();
+};
 };
 
 function concertThis(){
@@ -57,6 +60,8 @@ function concertThis(){
                     var dateTime = concertInfo[i].datetime.split('T')
                     var date = dateTime[0]
                     var newDate = moment(date).format("MM/DD/YYYY")
+                    var text = "\n" + "Search command:" + command + " " + input + "\n" + artist + " will be playing at " + concertInfo[i].venue.name + " in " + concertInfo[i].venue.city + ", " + concertInfo[i].venue.country + " on " + newDate + "\n"
+                    log(text)             
                     console.log(artist + " will be playing at " + concertInfo[i].venue.name + " in " + concertInfo[i].venue.city + ", " + concertInfo[i].venue.country + " on " + newDate)                   
                 }
                 console.log ("======================= Concert Information for " + artist + " =======================")
@@ -78,6 +83,9 @@ function spotifyThis(input){
     console.log ("======================= Song Information for " + input + " =======================")
     for (let i = 0; i < songNameLength; i++) {
        returnSong = songName[i]
+       var text = "\n" + "Search command:" + command + " " + input + "\n" + returnSong.name + " was released by " + returnSong.artists[0].name + " on the album " + returnSong.album.name + "." + "\n" + "Preview Url:" + returnSong.preview_url + "\n"
+       log(text)
+
     console.log(returnSong.name + " was released by " + returnSong.artists[0].name + " on the album " + returnSong.album.name + ".")
     console.log("Preview: " + returnSong.preview_url)
     console.log(" ")
@@ -85,7 +93,6 @@ function spotifyThis(input){
     console.log ("======================= Song Information for " + input + " =======================")
     console.log (" ")
 
-    // console.log(response.tracks.items);
   })
   .catch(function(err) {
     console.log("There's been an error: " + err);
@@ -97,6 +104,8 @@ function movieThis(input) {
     queryUrl = "http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=trilogy";
     axios.get(queryUrl).then(
         function(response) {
+            var text = "\n" + "Search command:" + command + " " + input + "\n" + " Movie Title: " + response.data.Title + "\n" + "Release Year: " + response.data.Year + "\n" + "IMDB Rating: " + response.data.imdbRating + "\n" + "Rotten Tomatoes Rating: " + response.data.Ratings[1].Value + "\n" + "Country Produced: " + response.data.Country + "\n" + "Language: " + response.data.Language + "\n" + "Plot: " + response.data.Plot + "\n" + "Actors: " + response.data.Actors + "\n"
+            log(text)
             console.log(" ")
             console.log("======================= Movie Information for " + input + " =======================")
             console.log(" ")
@@ -110,13 +119,35 @@ function movieThis(input) {
             console.log("Actors: " + response.data.Actors)
             console.log(" ")
             console.log("======================= Movie Information for " + input + " =======================")
-      
-
         })
         .catch(function(err) {
             console.log("There's been an error: " + err)
         })
 };
 
+function doWhatItSays(){
+    fs.readFile("random.txt", "utf8", function(error, data) {
 
+        if (error) {
+          return console.log(error);
+        }  
+        // Then split it by commas (to make it more readable)
+        var dataArr = data.split(",");
+      
+        command = dataArr[0]
+        input = dataArr[1]
+        liri(command, input)
+      });
+}
 
+function log(text){
+    fs.appendFile("log.txt", text, function(err) {
+        if (err) {
+            console.log(err)
+        }
+        else{
+            console.log("Search has been logged!");
+        }
+    });
+
+}
